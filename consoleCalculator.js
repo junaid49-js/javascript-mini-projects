7634e8cc155c1cd9faeb2b43817568f5fc92be1f
junaid1949 <junaidk175649@gmail.com>
2025-05-20 01:06:22 +0530

    initial calculator, will add command line later

---
+++ consoleCalculator.js
@@ -0,0 +1,27 @@
+let addition = function(a,b){
+    return a + b;
+}
+
+let substraction = function(a,b){
+    return a - b;
+}
+
+let multiply = function(a,b){
+    return a * b;
+}
+
+let division = function(a,b){
+    return a / b;
+}
+
+let remainder = function(a,b){
+    return a % b;
+}
+
+let squareOfX = function(x){
+    return x * x;
+}
+
+let powerBy = function(a,b){
+    return a ** b;
+}
