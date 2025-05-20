50d92cd10f21bc56c7ae486ebd73983b53679707
junaid1949 <junaidk175649@gmail.com>
2025-05-20 18:20:55 +0530

    initial commit

---
+++ tempConvert.js
@@ -0,0 +1,37 @@
+let celsiustoFahrenheit = function(C){
+    let F;
+    F = C * 9/5 + 32;
+    return F
+}
+
+let fahrenheitToCelsius = function(F){
+    let C;
+    C = (F - 32) * 5/9;
+    return C;
+}
+
+let kelvinToFahrenheit = function(K){
+    let F;
+    F = (K - 273.15) * 9/5 + 32;
+    return F;
+}
+
+let fahrenheitToKelvin = function(F){
+    let K;
+    K = ((F - 32) * 5/9) + 273.15;
+    return K;
+}
+
+let celsiustoKelvin = function(C){
+    let K;
+    K = C + 273.15;
+    return K;
+}
+
+let kelvinToCelsius = function(K){
+    let C;
+    C = K - 273.15;
+    return C;
+}
+
+console.log('You can convert the temperature value by this program.');
